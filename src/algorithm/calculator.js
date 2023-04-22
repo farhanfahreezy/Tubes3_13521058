489096cddbd47916d30eb323aa2bec48a3cbaf1d
Naufal-Nalendra <16521240@std.stei.itb.ac.id>
2023-04-22 18:26:56 +0700

    fix calculator

--- src/algorithm/calculator.js
+++ src/algorithm/calculator.js
@@ -1,70 +1,76 @@
 import createPromptSync from 'prompt-sync';
-import { evaluate, isComplex, complex } from 'mathjs';
 
 const prompt = createPromptSync({ autocomplete: false });
 
 function calculate(query) {
-  let components = query.replace(/\s/g, '').replace(/([\+\-\*\/\(\)])/g, ' $1 ').trim().split(/\s+/);
-  let stack = [];
-  let operators = [];
-  
-  for (let i = 0; i < components.length; i++) {
-    let component = components[i];
-    if (!isNaN(component)) {
-      stack.push(complex(Number(component)));
-    } else if (component === '(') {
-      operators.push('(');
-    } else if (component === ')') {
-      while (operators[operators.length-1] !== '(') {
-        let op = operators.pop();
-        let b = stack.pop();
-        let a = stack.pop();
-        stack.push(evaluate(`${a} ${op} ${b}`));
+    let components = query.replace(/\s/g, '').replace(/([\+\-\*\/\(\)])/g, ' $1 ').trim().split(/\s+/);
+    let stack = [];
+    let operators = [];
+    let balance = 0; // Keep track of the balance of opening and closing brackets
+    
+    for (let i = 0; i < components.length; i++) {
+      let component = components[i];
+      if (!isNaN(component)) {
+        stack.push(Number(component));
+      } else if (component === '(') {
+        operators.push('(');
+        balance++; // Increment the balance for each opening bracket
+      } else if (component === ')') {
+        while (operators[operators.length-1] !== '(') {
+          let op = operators.pop();
+          let b = stack.pop();
+          let a = stack.pop();
+          stack.push(eval(`${a} ${op} ${b}`));
+        }
+        operators.pop(); // Remove the '(' from the operators stack
+        balance--; // Decrement the balance for each closing bracket
+      } else if (precedence(component) > 0) {
+        while (operators.length > 0 && precedence(component) <= precedence(operators[operators.length-1])) {
+          let op = operators.pop();
+          let b = stack.pop();
+          let a = stack.pop();
+          stack.push(eval(`${a} ${op} ${b}`));
+        }
+        operators.push(component);
+      } else {
+        throw new Error(`Invalid operator: ${component}`);
       }
-      operators.pop(); // Remove the '(' from the operators stack
-    } else if (precedence(component) > 0) {
-      while (operators.length > 0 && precedence(component) <= precedence(operators[operators.length-1])) {
-        let op = operators.pop();
-        let b = stack.pop();
-        let a = stack.pop();
-        stack.push(evaluate(`${a} ${op} ${b}`));
-      }
-      operators.push(component);
-    } else {
-      throw new Error(`Invalid operator: ${component}`);
     }
-  }
-  
-  while (operators.length > 0) {
-    let op = operators.pop();
-    let b = stack.pop();
-    let a = stack.pop();
-    stack.push(evaluate(`${a} ${op} ${b}`));
-  }
-  
-  if (stack.length !== 1 || !isComplex(stack[0])) {
-    throw new Error('Invalid expression');
-  }
-  
-  return stack[0];
+    
+    while (operators.length > 0) {
+      let op = operators.pop();
+      let b = stack.pop();
+      let a = stack.pop();
+      stack.push(eval(`${a} ${op} ${b}`));
+    }
+    
+    if (stack.length !== 1 || isNaN(stack[0])) {
+      throw new Error('Invalid expression');
+    }
+    
+    if (balance !== 0) { // Check if the balance of brackets is not zero
+      throw new Error('Missing bracket');
+    }
+    
+    return stack[0];
 }
 
 function precedence(op) {
-  if (op === '+' || op === '-') {
-    return 1;
-  } else if (op === '*' || op === '/') {
-    return 2;
-  } else if (op === '^') {
-    return 3;
-  } else {
-    return 0;
-  }
+    if (op === '+' || op === '-') {
+      return 1;
+    } else if (op === '*' || op === '/') {
+      return 2;
+    } else if (op === '^') {
+      return 3;
+    } else {
+      return 0;
+    }
 }
 
 try {
-  let query = prompt('Enter an expression: ');
-  let result = calculate(query);
-  console.log(`The result is: ${result.toString()}`);
+    let query = prompt('Enter an expression: ');
+    let result = calculate(query);
+    console.log(`The result is: ${result.toString()}`);
 } catch (error) {
-  console.error(error.message);
+    console.error(error.message);
 }
